09f1899663b3f556d8cbada8774a719bbfcb423e
Phillip Wong <deepelegant99@gmail.com>
2025-06-12 18:13:37 -0700

    using render template

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request   
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    
+    return render_template("index.html", title="Home Page")
 
 if __name__ == "__main__":
-    app.run(debug=True) 
-
-
-
+    app.run(debug=True)
