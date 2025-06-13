5256f654de0d104b9e3cfd02b814de61420732ad
Phillip Wong <deepelegant99@gmail.com>
2025-06-12 23:47:54 -0700

    dynamatically acting on template

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    
-    return render_template("index.html", title="Home Page")
+    name = request.args["name"]
+    return render_template("index.html", name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
